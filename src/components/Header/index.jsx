acedd7d9cf9d1863252fc66dcbc202b645663157
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2024-05-16 11:23:11 +0300

    Update

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -8,18 +8,27 @@ export default function Header({ ...props }) {
         <div className="container-xs flex items-center justify-between gap-5 md:flex-col md:p-5">
           <Text as="p" className="self-end !text-white-A700">
             <span className="text-white-A700">
-              Sign up now and enjoy a 25% discount on your first order. Don&#39;t miss out!&nbsp;
+              Sign up now and enjoy a 25% discount on your first order.
+              Don&#39;t miss out!&nbsp;
             </span>
             <span className="font-semibold text-blue_700">Sign up now</span>
           </Text>
           <a href="#">
-            <Img src="images/img_icon.svg" alt="icon link" className="h-[24px] w-[24px] md:w-full" />
+            <Img
+              src="images/img_icon.svg"
+              alt="icon link"
+              className="h-[24px] w-[24px] md:w-full"
+            />
           </a>
         </div>
       </div>
       <div className="flex justify-center self-stretch border-b border-solid border-gray-600 bg-white-A700 py-6 sm:py-5">
         <div className="container-xs flex items-center justify-between gap-5 md:flex-col md:p-5">
-          <Img src="images/img_header_logo.svg" alt="header logo" className="h-[32px] w-[126px] object-contain" />
+          <Img
+            src="images/img_header_logo.svg"
+            alt="header logo"
+            className="h-[32px] w-[126px] object-contain"
+          />
           <ul className="flex flex-wrap gap-5">
             <li>
               <a href="#">
@@ -80,16 +89,32 @@ export default function Header({ ...props }) {
           </ul>
           <div className="flex gap-5">
             <a href="#">
-              <Img src="images/img_search.svg" alt="search icon" className="h-[24px] w-[24px]" />
+              <Img
+                src="images/img_search.svg"
+                alt="search icon"
+                className="h-[24px] w-[24px]"
+              />
             </a>
             <a href="#">
-              <Img src="images/img_favorite.svg" alt="favorite icon" className="h-[24px] w-[24px]" />
+              <Img
+                src="images/img_favorite.svg"
+                alt="favorite icon"
+                className="h-[24px] w-[24px]"
+              />
             </a>
             <a href="#">
-              <Img src="images/img_bag.svg" alt="bag icon" className="h-[24px] w-[24px]" />
+              <Img
+                src="images/img_bag.svg"
+                alt="bag icon"
+                className="h-[24px] w-[24px]"
+              />
             </a>
             <a href="#">
-              <Img src="images/img_lock.svg" alt="lock icon" className="h-[24px] w-[24px]" />
+              <Img
+                src="images/img_lock.svg"
+                alt="lock icon"
+                className="h-[24px] w-[24px]"
+              />
             </a>
           </div>
         </div>
